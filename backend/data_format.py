1028a020549d9a6e762fd05f49747d355624fe33
Juha Ylikoski <juha.ylikoski14@outlook.com>
2025-05-30 22:19:56 +0300

    Add response model

---
+++ backend/data_format.py
@@ -0,0 +1,35 @@
+from pydantic import BaseModel
+
+
+class PieChart(BaseModel):
+    chart_type = "pie"
+    shards: dict[str, float]
+
+
+class LineChart(BaseModel):
+    chart_type = "line"
+    series: dict[str, list[float]]
+
+
+class Response(BaseModel):
+    """Example:
+
+    ```
+    {
+      "title": "foo",
+      "text": "bar",
+      "chart": {
+        "chart_type": "bar",
+        "shards": {
+          "too much": 50.0,
+          "not enough": 50.0
+        }
+      }
+    }
+    ```
+
+    """
+
+    title: str
+    text: str
+    chart: BarChart | LineChart
