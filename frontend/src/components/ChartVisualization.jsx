76d7e5062de868183dee36c0bec17babe99b328c
vesa haaparanta <vesahaaparanta@hotmail.fi>
2025-05-31 19:55:52 +0300

    .

--- frontend/src/components/ChartVisualization.jsx
+++ frontend/src/components/ChartVisualization.jsx
@@ -20,6 +20,7 @@ export default function ChartVisualization({ chart }) {
     const height = 300;
     const margin = { top: 20, right: 20, bottom: 20, left: 40 };
 
+    
     // Create SVG
     const svg = container
       .append("svg")
